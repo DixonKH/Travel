902ec7b6d03ed936d265d6235cc5d7e1cb30195b
DixonKH <khaydarovdilshod9@gmail.com>
2024-10-17 22:03:29 +0900

    Handling Form Submission

--- src/app/components/CreateProduct/CreateForm.js
+++ src/app/components/CreateProduct/CreateForm.js
@@ -1,66 +1,79 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function CreateForm() {
-    let [pName, updateName] = useState('');
-    let [pPrice, updatePrice] = useState('');
-    let [pDesc, updateDesc] = useState('');
-    let [pAvailability, updateAvailability] = useState('');
-    let [pImageUrl, updateImageUrl] = useState('');
+  let [pName, updateName] = useState("");
+  let [pPrice, updatePrice] = useState("");
+  let [pDesc, updateDesc] = useState("");
+  let [pAvailability, updateAvailability] = useState("");
+  let [pImageUrl, updateImageUrl] = useState("");
 
-    //  let [userInput, updateUserInput] = useState({
-    //     pName: '',
-    //     pPrice: '',
-    //     pDesc: '',
-    //     pAvailability: '',
-    //     pImageUrl: ''   
-    //  });
+  //  let [userInput, updateUserInput] = useState({
+  //     pName: '',
+  //     pPrice: '',
+  //     pDesc: '',
+  //     pAvailability: '',
+  //     pImageUrl: ''
+  //  });
 
-    function nameInputHandler(event) {
-        updateName(event.target.value);
+  function nameInputHandler(event) {
+    updateName(event.target.value);
     //    updateUserInput({
     //     ...userInput,
     //     pName: event.target.value
     // });
-//     updateUserInput((prevState) => {
-//         return {...prevState, pName: event.target.value};
-//    })
-    }
+    //     updateUserInput((prevState) => {
+    //         return {...prevState, pName: event.target.value};
+    //    })
+  }
 
-    function priceInputHandler(event) {
-        updatePrice(event.target.value);
-        // updateUserInput({
-        //     ...userInput,
-        //     pPrice: event.target.value
-        // });
-     }
+  function priceInputHandler(event) {
+    updatePrice(event.target.value);
+    // updateUserInput({
+    //     ...userInput,
+    //     pPrice: event.target.value
+    // });
+  }
 
-     function descriptionInputHandler(event) {
-       updateDesc(event.target.value);
-        // updateUserInput({
-        //     ...userInput,
-        //     pDesc: event.target.value
-        // });
-     }
+  function descriptionInputHandler(event) {
+    updateDesc(event.target.value);
+    // updateUserInput({
+    //     ...userInput,
+    //     pDesc: event.target.value
+    // });
+  }
 
-     function availabilityInputHandler(event) {
-        updateAvailability(event.target.checked);
-        // updateUserInput({
-        //     ...userInput,
-        //     pAvailability: event.target.value
-        // });
-     }
+  function availabilityInputHandler(event) {
+    updateAvailability(event.target.checked);
+    // updateUserInput({
+    //     ...userInput,
+    //     pAvailability: event.target.value
+    // });
+  }
 
-     function imageInputHandler(event) {
-        updateImageUrl(event.target.value);
-        // updateUserInput({
-        //     ...userInput,
-        //     pImageUrl: event.target.value
-        // });
-     }
+  function imageInputHandler(event) {
+    updateImageUrl(event.target.value);
+    // updateUserInput({
+    //     ...userInput,
+    //     pImageUrl: event.target.value
+    // });
+  }
+
+  function createProductEventHandler(event) {
+    event.preventDefault();
+    let product = {
+      id: 1,
+      name: pName,
+      desc: pDesc,
+      price: Number(pPrice),
+      isAvailable: Boolean(pAvailability),
+      image: pImageUrl,
+    };
+    console.log(product);   
+  }
 
   return (
-    <form className="row g-3 p-3 shadow">
-      <div className="col-md-6"> 
+    <form className="row g-3 p-3 shadow" onSubmit={createProductEventHandler}>
+      <div className="col-md-6">
         <label for="name">Product Name</label>
         <input
           type="text"
@@ -109,7 +122,12 @@ function CreateForm() {
 
       <div className="form-group">
         <label for="select-image">Select product image</label>
-        <input type="file" className="form-control" id="select-image"  onChange={imageInputHandler} />
+        <input
+          type="file"
+          className="form-control"
+          id="select-image"
+          onChange={imageInputHandler}
+        />
       </div>
 
       <button type="submit" className="btn btn-primary">
